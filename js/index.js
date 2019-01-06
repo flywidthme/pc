a478eb60e38a9821eaf534dbf165e8a0a2fa4e72
qiangqiang <1070025626@qq.com>
2019-01-06 17:18:03 +0800

    '完成内容区滚动'

--- js/index.js
+++ js/index.js
@@ -1,13 +1,16 @@
 //等待页面（所有资源）加载完成，
 window.onload = function(){
+    //获取dom元素
+    var arrowNode = document.querySelector('.arrow');
+    var headerLisNodes = document.querySelectorAll('.nav li');
+    var headerDownNodes = document.querySelectorAll('.down');
+    var contentUlNode = document.querySelector('.content-main');
+    var contentNode = document.querySelector('.content');
+    var contentHeight = contentNode.offsetHeight;
+    var nowIndex = 0;
     headerArea();
+    //头部区域功能
     function headerArea(){
-        //获取dom元素
-        var arrowNode = document.querySelector('.arrow');
-        var headerLisNodes = document.querySelectorAll('.nav li');
-        var headerDownNodes = document.querySelectorAll('.down');
-
-
         //初始化时，小箭头默认在第一个li上
         arrowNode.style.left = headerLisNodes[0].getBoundingClientRect().left+headerLisNodes[0].offsetWidth/2
             -arrowNode.offsetWidth/2+'px';
@@ -19,13 +22,65 @@ window.onload = function(){
             headerLisNodes[i].index = i;
 
             headerLisNodes[i].onclick = function(){
-                for (var j = 0; j <headerDownNodes.length; j++) {
-                    headerDownNodes[j].style.width='0';
-                }
-                arrowNode.style.left = this.getBoundingClientRect().left+this.offsetWidth/2-arrowNode.offsetWidth/2+'px';
-                //设置当前的宽度为100%
-                headerDownNodes[this.index].style.width = '100%';
+                //点击时，同步更新nowIndex；否则点击后滚动容易出问题
+                nowIndex = this.index;
+               move(nowIndex);
             }
         }
+    }
+
+    function move(nowIndex){
+        //其他div的宽度为0
+        for (var j = 0; j <headerDownNodes.length; j++) {
+            headerDownNodes[j].style.width='0';
+        }
+        arrowNode.style.left = headerLisNodes[nowIndex].getBoundingClientRect().left+headerLisNodes[nowIndex].offsetWidth/2-arrowNode.offsetWidth/2+'px';
+        //设置当前的宽度为100%
+        headerDownNodes[nowIndex].style.width = '100%';
+        //关联头部ul和轮播
+        contentUlNode.style.top = -nowIndex*contentHeight+'px';
+    }
+    //滚轮事件
+    document.onmousewheel = wheel;
+    document.addEventListener('DOMMouseScroll',wheel);
+    function wheel(event) {
+        event = event || window.event;
+
+        var flag = '';
+        if (event.wheelDelta) {
+            //ie/chrome
+            if (event.wheelDelta > 0) {
+                flag = 'up';
+            } else {
+                flag = 'down'
+            }
+        } else if (event.detail) {
+            //firefox
+            if (event.detail < 0) {
+                flag = 'up';
+            } else {
+                flag = 'down'
+            }
+        }
+
+        switch (flag) {
+            case 'up' :
+                if (nowIndex>0) {
+                    nowIndex--;
+                    move(nowIndex);
+                }
+
+                break;
+            case 'down' :
+                if (nowIndex<4) {
+                    nowIndex++;
+                    move(nowIndex);
+                }
+                break;
+        }
+
+        //禁止默认行为
+        event.preventDefault && event.preventDefault();
+        return false;
     }
 }
