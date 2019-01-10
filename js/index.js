4aa9e9e209b3016098bf5ac097b19e99baac7774
qiangqiang <1070025626@qq.com>
2019-01-10 16:22:05 +0800

    complete 第五屏

--- js/index.js
+++ js/index.js
@@ -144,8 +144,122 @@ window.onload = function(){
         },2500)
     }
 
-    
-    
+    //最后一屏
+    lastView();
+    function lastView(){
+      var teamUlNode = document.querySelector('.team .team-person');
+      var teamLiNodes = document.querySelectorAll('.team .team-person li');
+      var createCircleTimer = null;
+      var paintingTimer = null;
+      var canvas = null;
+    //  设置画布的宽高
+      var width = teamLiNodes[0].offsetWidth;
+      var height = teamLiNodes[0].offsetHeight;
+      //  给每个li绑定鼠标移入事件
+      for (var i = 0; i <teamLiNodes.length; i++) {
+        teamLiNodes[i].ind = i;
+        teamLiNodes[i].onmouseenter = function(){
+          //改变透明度
+          for (var j = 0; j < teamLiNodes.length; j++) {
+            teamLiNodes[j].style.opacity = 0.5;
+          }
+          this.style.opacity = 1;
+          if(!canvas){
+            canvas = document.createElement('canvas');
+            canvas.width = width;
+            canvas.height = height;
+
+            canvas.className = 'canvas';
+            bubble(canvas);
+            teamUlNode.appendChild(canvas);
+          }
+          //不管填不添加画布，都会移动
+          canvas.style.left = this.ind*width+'px';
+        }
+      }
+      teamUlNode.onmouseleave = function(){
+        createCircleTimer = null;
+        paintingTimer = null;
+        for (var i = 0; i < teamLiNodes.length; i++) {
+          teamLiNodes[i].style.opacity = 1;
+        }
+        canvas.remove();
+        canvas = null;
+      }
+      function bubble(canvas) {
+        if (canvas.getContext) {
+          var ctx = canvas.getContext('2d');
+
+          var circleArr = [];
+
+          //生成随机圆
+          createCircleTimer = setInterval(function () {
+            //颜色随机
+            var r = Math.round(Math.random() * 255);
+            var g = Math.round(Math.random() * 255);
+            var b = Math.round(Math.random() * 255);
+            //半径随机
+            var c_r = Math.round(Math.random() * 8 + 2);
+            //缩放系数
+            var s = Math.round(Math.random() * 50 + 50);
+            //起始位置 x轴坐标随机 y轴坐标不变
+            var x = Math.round(Math.random() * canvas.width);
+            var y = canvas.height + c_r;
+
+            //添加到数组中
+            circleArr.push({
+              r: r,
+              g: g,
+              b: b,
+              c_r: c_r,
+              s: s,
+              x: x,
+              y: y,
+              d: 0  //角度
+            })
+
+          }, 20)
+
+          //画圆
+          paintingTimer = setInterval(function () {
+            //清除画布, 清除上一次画的东西
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+            for (var i = 0; i < circleArr.length; i++) {
+              //获取单个圆
+              var item = circleArr[i];
+              //角度增加
+              item.d += 4;
+              //计算得出弧度
+              var rad = item.d * Math.PI / 180;
+              //计算圆心的坐标
+              var x = Math.round(item.x + Math.sin(rad) * item.s);
+              var y = Math.round(item.y - rad * item.s);
+              //判断y轴的坐标，如果小于0，清除掉
+              if (y <= 0) {
+                circleArr.splice(i, 1);
+                continue;
+              }
+
+              //画圆
+              ctx.fillStyle = 'rgb(' + item.r + ',' + item.g + ',' + item.b + ')';
+              ctx.beginPath();
+              ctx.arc(x, y, item.c_r, 0, 2 * Math.PI);
+              ctx.fill();
+            }
+
+          }, 1000 / 60)
+
+        }
+      }
+
+
+
+
+
+
+
+    }
     
     
 }
